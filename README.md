9fcaa7168783b7b9d403f12bbdc02f0517459ca8
paulosky311 <pfernandoy@gmail.com>
2025-03-09 17:12:36 -0300

    Agrega el proyecto en Java

---
+++ README.md
@@ -0,0 +1,17 @@
+# Título del Proyecto
+
+<h1>Challenger Amigo Secreto</h1>
+
+# Autor
+
+<h2>Paulo Fernandoy</h2>
+
+# Software utilizados
+
+Java Script
+HTML
+CSS
+
+# Agradecimientos
+
+Gracias a todos en Alura por la oportunidad de aprender 🍺
--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Variables
+let amigo = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+
+    if (!nombreAmigo) {
+        alert("Debes ingresar un nombre de un amigo");
+        return;
+    }
+    amigo.push(nombreAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    renderizarAmigos();
+}
+
+function renderizarAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+    
+    for (let i = 0; i < amigo.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigo[i];
+        listaAmigos.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigo.length == 0) {
+        alert("No hay amigos para sortear");
+        return;
+    }
+    let amigoGanador = amigo[Math.floor(Math.random() * amigo.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo ganador es: ${amigoGanador}`;
+
+    let limpiarLista = document.getElementById("listaAmigos");
+    limpiarLista.innerHTML = "";
+    amigo = []; 
+}
